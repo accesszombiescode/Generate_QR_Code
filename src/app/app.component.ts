21c66be31f3cfd413347d66600c187c76167724d
AccessZombiesCode <learnforestcode@gmail.com>
2022-05-15 13:15:25 +0530

    Generate_QRCode_repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,32 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'Generate_QR_Code';
+
+public values!:string;
+public level!: "L" | "M" | "Q" | "H";
+public width!: number; 
+public date:any = new Date(new Date().getTime()).toLocaleTimeString() + new Date(new Date().getTime()).toLocaleDateString();  
+
+constructor()
+{
+  this.level ="L";
+  this.width =200;
+  this.values = "QR Code By Access Zombies Code" + " " + this.date;
+}
+
+qrLevel(val: "L" | "M" | "Q" | "H") {
+  this.level = val;
+}
+
+qrData(val: string) {
+  this.values = val ;
+}
+
+qrWidth(val: number) {
+  this.width = val;
+}
 }
